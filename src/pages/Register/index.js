4f6eceaa8b04bbe9491aee13626ed9b75bfb3037
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-03-29 15:54:51 -0300

    Base of the project together with the first page (LOGIN)

---
+++ src/pages/Register/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+import './styles.css';
+
+export default class Register extends Component{
+    render(){
+        return(
+            <h1>Registr</h1>
+        )
+    }
+} 
